c5a1b52dc582980a6db5917cf2524a1072de6335
Henry He <heyongneng@nowcoder.com>
2022-04-19 00:03:10 +0800

    md

--- src/pages/app/Layout.vue
+++ src/pages/app/Layout.vue
@@ -1,6 +1,15 @@
 <template lang='pug'>
   el-container#app
-    h2 xx
+    el-header#header
+      el-menu(mode='horizontal' background-color='rgb(80, 158, 227)' text-color='#fff')
+        el-menu-item 新建查询
+        el-menu-item 浏览数据
     el-main#main-body: transition(name='el-fade-in-linear' mode='out-in')
       router-view
 </template>
+
+<style lang='less'>
+  #header {
+    background-color: rgb(80, 158, 227);
+  }
+</style>
